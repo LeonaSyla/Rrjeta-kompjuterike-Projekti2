b5ad1d1a9c959b18895243fe37398e5b629fa9a3
leonitamataj <90731457+leonitamataj@users.noreply.github.com>
2022-04-11 20:03:20 +0200

    Add files via upload

--- client.php
+++ client.php
@@ -1,6 +1,14 @@
 <?php
+set_time_limit(0);
+
+$host = "127.0.0.1";
+$port =9999 ;
+
+$socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("socket_create fail\n");
+$result = socket_connect($socket, $host, $port) or die("socket_connect fail\n");
 
 
+echo "Eshte arritur lidhja. \n\n";
 
-
-?>
+while (true){
+ 
